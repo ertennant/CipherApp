f1010da685196739b92499e71f0f2a0db023278c
ertennant <elizabethtennant1@gmail.com>
2025-02-12 20:29:52 -0500

    Added GitHub Pages config.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+// Config for GitHub Pages 
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true, 
+  }, 
+  assetPrefix: isProd ? "/CipherApp" : "",
+  basePath: isProd ? "/CipherApp" : "",
+  output: "export",
 };
 
 export default nextConfig;
