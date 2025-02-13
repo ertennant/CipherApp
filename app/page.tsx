5612e5dd9e23ed246432452746548e0c3fd34f7f
ertennant <elizabethtennant1@gmail.com>
2025-02-13 12:43:29 -0500

    Routing.

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,71 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import TextPanel from './ui/text-panel';
+import CipherControlPanel from './ui/cipher-control-panel';
+import { useState } from 'react';
+import RadioButtonGroup from './ui/radio-button-group';
+
+export default function Page() {
+  const [originalText, setOriginalText] = useState("");
+  const [currentText, setCurrentText] = useState("");
+  const [mode, setMode] = useState("encrypt");
+  const [activePanel, setActivePanel] = useState("input");
+  const [lastActiveText, setLastActiveText] = useState("input");
+
+  function handleInputText(text: string) {
+    console.log(`called handleInputText(${text})`);
+    setOriginalText(text);
+    setCurrentText(text);
+    setLastActiveText("output");
+    setActivePanel("settings");
+    console.log(`lastActiveText = ${lastActiveText}, activePanel = ${activePanel}`);
+  }
+
+  function handleUpdateText(text: string) {
+    console.log(`called handleUpdateText(${text})`);
+    setCurrentText(text);
+    setLastActiveText("output");
+    setActivePanel("output");
+    console.log(`lastActiveText = ${lastActiveText}, activePanel = ${activePanel}`);
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    <main className="text-text-normal h-full grid content-start grid-rows-12 md:grid-flow-col md:grid-cols-5 bg-primary/55 dark:bg-primary/75 backdrop-blur-md shadow-[0_8px_10px_-5px] shadow-slate-800/40">
+      <RadioButtonGroup
+        active={mode}
+        options={["encrypt", "decrypt"]}
+        onChange={setMode}
+      >
+      </RadioButtonGroup>
+      <TextPanel
+        title={mode === "encrypt" ? "Plaintext" : "Ciphertext"}
+        content={originalText}
+        placeholder="Enter your message here."
+        readOnly={false}
+        // isExpanded={activePanel === "input" ? 2 : activeText === "input" ? 1 : 0}
+        onSubmit={handleInputText}
+        onClick={() => {setActivePanel("input"); setLastActiveText("input");}}
+        visibility={activePanel === "input" ? "all" : lastActiveText === "input" ? "md" : "xl"}
+      >
+      </TextPanel>
+      <TextPanel
+        title={mode === "encrypt" ? "Ciphertext" : "Plaintext"}
+        content={currentText}
+        // isExpanded={activePanel === "output" ? 2 : activeText === "output" ? 1 : 0}
+        placeholder="Result will be displayed here."
+        readOnly={true}
+        onClick={() => {setActivePanel("output"); setLastActiveText("output");}}
+        visibility={activePanel === "output" ? "all" : lastActiveText === "output" ? "md" : "xl"}
+      >
+      </TextPanel>
+      <CipherControlPanel 
+        originalText={originalText}
+        currentText={currentText}
+        isExpanded={activePanel === "settings"}
+        onClick={() => setActivePanel("settings")}
+        onUpdateText={handleUpdateText}
+        mode={mode}
+      />
+    </main>
+  )
 }
